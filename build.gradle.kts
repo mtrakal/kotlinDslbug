09a5db46a10ed0c6d30942e3eede8b1787d7721a
Matěj Trakal <mtrakal@gmail.com>
2018-12-08 18:15:46 +0100

    only once working solution after change to KTS file

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,16 +7,16 @@ buildscript {
     }
     dependencies {
         // FIXME: THIS WORKS
-//        classpath "com.android.tools.build:gradle:3.2.1"
-//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11"
+        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.11")
 
         // FIXME: THIS DIDN'T WORK when use KTS. When I was using build.gradle file, it works
 //        classpath("com.android.tools.build:gradle:${Config.Versions.gradleBuildTools}")
 //        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Config.Versions.kotlinVersion}")
 
         // FIXME: THIS DIDN'T WORK when use KTS. When I was using build.gradle file, it works
-        classpath(Config.BuildPlugins.androidPlugin)
-        classpath(Config.BuildPlugins.kotlinPlugin)
+//        classpath(Config.BuildPlugins.androidPlugin)
+//        classpath(Config.BuildPlugins.kotlinPlugin)
     }
 }
 
@@ -27,9 +27,10 @@ allprojects {
     }
 }
 
-wrapper {
+// tasks.withType(Wrapper::class.java).configureEach {
+tasks.withType<Wrapper> {
     // https://services.gradle.org/distributions/
-    gradleVersion = '5.0'
+    gradleVersion = "5.0"
 
     distributionUrl = distributionUrl.replace("bin", "all")
 }
